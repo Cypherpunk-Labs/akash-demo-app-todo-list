f54a28c91b55b44f670b9fb89d484d3d66a1cf0b
kempy007 <13536174+kempy007@users.noreply.github.com>
2023-09-20 15:27:39 +0100

    Patch1

--- flask-sqlalchemy-sqlite/setup-database.sh
+++ flask-sqlalchemy-sqlite/setup-database.sh
@@ -7,3 +7,19 @@ cockroach sql -e 'CREATE DATABASE example_flask_sqlalchemy'
 cockroach sql -e 'GRANT ALL ON DATABASE example_flask_sqlalchemy TO example'
 
 python -c 'import hello; hello.db.create_all()'
+
+
+
+# sqlalchemy.exc.OperationalError: (sqlite3.OperationalError) no such table: todos
+# [SQL: SELECT todos.todo_id AS todos_todo_id, todos.title AS todos_title, todos.text AS todos_text, todos.done AS todos_done, todos.pub_date AS todos_pub_date 
+# FROM todos ORDER BY todos.pub_date DESC]
+# (Background on this error at: http://sqlalche.me/e/14/e3q8)
+
+# CREATE TABLE todos (
+# 	todo_id INTEGER PRIMARY KEY,
+#    	title TEXT NOT NULL,
+# 	text TEXT NOT NULL,
+#     done INTEGER DEFAULT 0,
+#     pub_date TEXT NOT NULL,
+# 	table_constraints
+# );
